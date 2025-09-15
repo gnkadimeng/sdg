1f2b15d367f7044f21c244649f21829e593dfedf
cktshukudu <cktshukudu@gmail.com>
2025-09-15 20:32:09 +0200

    changes

--- config.py
+++ config.py
@@ -5,20 +5,20 @@ SECRET_KEY = os.getenv("SECRET_KEY", "6ce255c00e63d46c5af17afd44fdccb3")
 
     
     # Online database
-def get_db_connection():
-    try:
-        conn = psycopg2.connect(
-            dbname=os.getenv("DB_NAME", "randrefinerydb_hdcz"),  
-            user=os.getenv("DB_USER", "randrefinerydb_hdcz_user"),    
-            password=os.getenv("DB_PASSWORD", "NJY9sBdbbw3Sipd0gFGhHFjlLoiWnaaD"),  
-            host=os.getenv("DB_HOST", "dpg-cudl8flumphs73cpbcj0-a"),  
-            port=os.getenv("DB_PORT", "5432")  
-        )
-        print("✅ Database Connection Successful!")
-        return conn
-    except Exception as e:
-        print(f"❌ Database Connection Error: {e}")
-        return None
+# def get_db_connection():
+#     try:
+#         conn = psycopg2.connect(
+#             dbname=os.getenv("DB_NAME", "randrefinerydb_hdcz"),  
+#             user=os.getenv("DB_USER", "randrefinerydb_hdcz_user"),    
+#             password=os.getenv("DB_PASSWORD", "NJY9sBdbbw3Sipd0gFGhHFjlLoiWnaaD"),  
+#             host=os.getenv("DB_HOST", "dpg-cudl8flumphs73cpbcj0-a"),  
+#             port=os.getenv("DB_PORT", "5432")  
+#         )
+#         print("✅ Database Connection Successful!")
+#         return conn
+#     except Exception as e:
+#         print(f"❌ Database Connection Error: {e}")
+#         return None
 
 
 #offline database
@@ -36,19 +36,19 @@ def get_db_connection():
 #         print("Error connecting to PostgreSQL database:", e)
 #         return None
     
-# def get_db_connection():
-#     try:
-#         conn = psycopg2.connect(
-#             dbname=os.getenv("DB_NAME", "sustainability_app"),
-#             user=os.getenv("DB_USER", "postgres"),
-#             password=os.getenv("DB_PASSWORD", "Musa"),
-#             host=os.getenv("DB_HOST", "localhost"),
-#             port=os.getenv("DB_PORT", "5432")
-#         )
-#         return conn
-#     except Exception as e:
-#         print("Error connecting to PostgreSQL database:", e)
-#         return None
+def get_db_connection():
+    try:
+        conn = psycopg2.connect(
+            dbname=os.getenv("DB_NAME", "sustainability_app"),
+            user=os.getenv("DB_USER", "postgres"),
+            password=os.getenv("DB_PASSWORD", "Musa"),
+            host=os.getenv("DB_HOST", "localhost"),
+            port=os.getenv("DB_PORT", "5432")
+        )
+        return conn
+    except Exception as e:
+        print("Error connecting to PostgreSQL database:", e)
+        return None
 
 
 ADMIN_EMAIL = "admin@example.com"
